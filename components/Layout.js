5d688ec5eef06817439a5c4f10a8506d1487eded
paruj_lab <paruj.lab@gmail.com>
2023-01-29 03:27:43 +0700

    feat:fix material ui server side rendering(SSR)

--- components/Layout.js
+++ components/Layout.js
@@ -1,20 +1,22 @@
 import React from "react";
 import Head from "next/head";
 import { AppBar, Toolbar, Typography, Container } from "@material-ui/core";
+import useStyles from "@/utils/styles";
 
 export default function Layout({ children }) {
+    const classess = useStyles();
   return (
     <div>
       <Head>
         <title>Next Amazon</title>
       </Head>
-      <AppBar position="static">
+      <AppBar position="static" className={classess.navbar}>
         <Toolbar>
           <Typography>amazon</Typography>
         </Toolbar>
       </AppBar>
-      <Container>{children}</Container>
-      <footer>
+      <Container className={classess.main}>{children}</Container>
+      <footer className={classess.footer}>
         <Typography>All right reserved. Next Amazona.</Typography>
       </footer>
     </div>
--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,6 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { ServerStyleSheets } from "@material-ui/core/styles";
+import { Html, Head, Main, NextScript } from "next/document";
+import React from "react";
 
 export default function Document() {
   return (
@@ -9,5 +11,25 @@ export default function Document() {
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
+
+// fix material ui server side rendering(SSR)
+Document.getIntialProps = async (ctx) => {
+  const sheets = new ServerStyleSheets();
+  const originalRenderPage = ctx.renderPage;
+  ctx.renderPage = () => {
+    return originalRenderPage({
+      enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
+    });
+  };
+
+  const initialProps = await Document.getIntialProps(ctx);
+  return {
+    ...initialProps,
+    styles: [
+      ...React.Children.toArray(initialProps.styles),
+      sheets.getStyleElement(),
+    ],
+  };
+};
